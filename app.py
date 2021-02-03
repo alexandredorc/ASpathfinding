230c932846bcf0b8483257eb4e7480b3f691ad55
alexandredorc <ddorcival@gmail.com>
2021-02-03 20:51:11 +0100

    draw maze

--- app.py
+++ app.py
@@ -1,56 +1,158 @@
 import pygame as pg
 import sys
 
-maps=[['0','0','0','0','0','0','0','0','0','0'],
-    ['0','0','0','0','0','0','0','0','B','0'],
-    ['0','0','0','0','0','0','0','0','0','0'],
-    ['0','0','0','0','0','0','0','0','0','0'],
-    ['0','0','0','0','0','0','0','0','0','0'],
-    ['0','0','0','0','0','0','0','0','0','0'],
-    ['0','0','A','0','0','0','0','0','0','0'],
-    ['0','0','0','0','0','0','0','0','0','0'],
-    ['0','0','0','0','0','0','0','0','0','0'],
-    ['0','0','0','0','0','0','0','0','0','0']]
-    
-print(maps)
+
+
+
+posS=(2,4)
+posE=(7,9)
 pg.init()
 size=1000
+rows=200
+width=size//rows
 screen = pg.display.set_mode((size, size))
 
 white=(255,255,255)
-red=(255,0,0)
-green=(0,255,0)
-blue=(0,0,255)
-font = pg.font.Font('freesansbold.ttf', 20)
-
-screen.fill((12,34,56))
-for i in range(1, 10):
-    pg.draw.line(screen,white,(size*i//10,0),(size*i//10,size))
-    pg.draw.line(screen,white,(0,size*i//10),(size,size*i//10))
-for x in range(0,10):
-    for y in range(0,10):
-        if maps[y][x]!='0':
-            pg.draw.rect(screen,red,(x*size/10+1,y*size//10+1,size//10 -1,size//10-1))
-            img = font.render(maps[y][x], True, blue)
-            rect=img.get_rect()
-            rect.center=(x*10,y*10)
-            print(rect)
-            screen.blit(img,)
-           
+red=(162,15,15)
+green=(30,170,15)
+blue=(15,40,170)
+black=(0,0,0)
+cyanS=(75,200,200)
+cyanE=(75,201,200)
+wall=(12,34,56)
+grey=(125,125,125)
 
 
+#preset pos
+
+class Node: 
+    def __init__(self,row, col, width,total):
+        self.row = row
+        self.colum = col
+        self.x = row * width
+        self.y = col * width
+        self.color = white
+        self.neighbords=[]
+        self.width = width
+        self.total = total
+
+    def getpos(self):
+        return self.row, self.col
+
+    def is_state(self,color):
+        return self.color == color
+
+    def changeColor(self,color):
+        self.color = color
+
+    def draw(self,screen):
+        pg.draw.rect(screen,self.color,(self.x, self.y, self.width, self.width ))
+
+    def update(self,grid):
+        pass
+
+    def __lt__(self,other):
+        return False
+
+def dist(p1,p2):
+    x1,y1= p1
+    x2,y2= p2
+    x=abs(x2-x1)
+    y=abs(y2-y1)
+    d=max(x,y)-abs(x-y)
+    return d*14 + (x-d)*10 + (y-d)*10
 
 
-#GameLoop
-running = True
-while running:
+def make_grid(rows,width):
+    grid = []
+    gap = width // rows
+    for i in range(rows):
+        grid.append([])
+        for j in range(rows):
+            node = Node(i,j,gap,rows)
+            grid[i].append(node)
 
+    return grid 
 
+def draw_grid(screen, rows,width):
+    gap = width // rows
+    for i in range(rows):
+        pg.draw.line(screen,grey,(0,i*gap),(width,i*gap))
+        pg.draw.line(screen,grey,(i*gap,0),(i*gap,width))
 
-
-
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
-
+def draw(screen,grid,rows,width):
+    screen.fill(white)
+    for row in grid:
+        for node in row:
+            node.draw(screen)
+    draw_grid(screen,rows,width)
     pg.display.update()
+
+def get_pos(pos,rows,size):
+    x,y=pos
+    return(x*rows//size,y*rows//size)
+
+def change(pos,color,grid):
+    grid[pos[0]][pos[1]].changeColor(color);
+
+def casePath(node):
+    pass
+
+def pathfinding(grid):
+    for row in grid:
+        for node in row:
+            node= casePath(node)
+            
+def pen(pos,color,grid,stroke):
+    for x in range(-1*stroke+1,stroke):
+        for y in range(-1*stroke+1,stroke):
+            if pos[0]+x>=0 and pos[0]+x<rows and pos[1]+y>=0 and pos[1]+y<rows and not grid[pos[0]+x][pos[1]+y].is_state(cyanE) and not grid[pos[0]+x][pos[1]+y].is_state(cyanS):
+                change((pos[0]+x,pos[1]+y),color,grid)
+            
+
+def main():
+    
+    state= 'walls'
+    grid=make_grid(rows,size)
+
+    change(posE,cyanE,grid)
+    change(posS,cyanS,grid)
+    stroke=1
+    pg.display.update()
+    #GameLoop
+    run = True
+    while run:
+        draw(screen,grid,rows,size)
+
+        
+        keys=pg.key.get_pressed()
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                run = False
+        
+        if state == 'walls':
+            if pg.mouse.get_pressed() ==(1,0,0):
+                pos = get_pos(pg.mouse.get_pos(),rows,size)
+                if keys[pg.K_LSHIFT]:
+                    pen(pos,white,grid,stroke)
+                else:
+                    pen(pos,wall,grid,stroke)
+            x,y = get_pos(pg.mouse.get_pos(),rows,size)
+            if keys[pg.K_1]:
+                stroke=1
+            if keys[pg.K_2]:
+                stroke=2
+            if keys[pg.K_3]:
+                stroke=3
+            if keys[pg.K_4]:
+                stroke=4
+            if keys[pg.K_RETURN]:
+                state = 'finding'
+        elif state == 'finding':
+            pass
+        
+        pg.display.update()
+    pg.quit()
+
+
+main()
