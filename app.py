eb6e4581385e8c43e870364b49238e283015680a
alexandre dorcival <ddorcival@gmail.com>
2021-11-30 19:37:33 +0100

    correction and add weighted A* algorithme

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import pygame as pg
 import sys
-
-
-
-
+from time import sleep
 
 pg.init()
 size=800
@@ -105,27 +102,29 @@ def pen(pos,color,grid,stroke):
 
 def activate(node,origin,posx,posy,posE):
     if  node.gainS ==-1:
+        node.origin=origin
         if posy != 0 and posx != 0:
             node.gainS=origin.gainS+14
         else:
             node.gainS=origin.gainS+10
     elif posy != 0 and posx != 0 :
         if node.gainS>origin.gainS+14:
+            node.origin=origin
             node.gainS=origin.gainS+14
     else:
         if node.gainS > origin.gainS+10:
+            node.origin=origin
             node.gainS=origin.gainS+10
 
     
     node.gainE=dist((node.row,node.colum),posE)
 
-
-    node.totgain=node.gainE+node.gainS
+    
+    node.totgain=(node.gainE*1.5)+node.gainS
    
         
     
 def activatearound(grid,pos,posE,posS):
-    finish=0
     if pos!=posE and pos!=posS:
         change((pos[0],pos[1]),red,grid)
     for i in range(-1,2):
@@ -135,31 +134,52 @@ def activatearound(grid,pos,posE,posS):
                 if grid[pos[0]+i][pos[1]+j].color!=red:
                     grid[pos[0]+i][pos[1]+j].color=green
             elif (pos[0]+i,pos[1]+j)==posE:
-                finish=1
-    return finish
+                return grid[pos[0]][pos[1]]
+    return False
           
                 
             
 
 def algo(grid,posE,posS):
     finish=0
-    best = grid[0][0]
+    best = Node(-1, -1, -1,-1)
     best.totgain=-1
+    totgain=-1
+    lisBest=[]
     for row in grid:
         for node in row:
             if node.color==green:
-                if node.totgain < best.totgain or best.totgain==-1 :
-                    best=node
+                if node.totgain < totgain or totgain==-1 :
+                    lisBest=[]
+                    lisBest.append(node)
+                    totgain=node.totgain
+                if node.totgain == best.totgain:
+                    lisBest.append(node)
                 if node.row==posE[0] and node.colum==posE[1]:
                     return 1
-    if best.totgain==-1:
-        return -1
-    else:
-        finish =activatearound(grid,(best.row,best.colum),posE,posS)
+    
+    mini = Node(-1, -1, -1,-1)
+    mini.gainE =-1
+    for i,node in enumerate(lisBest):
+        if node.gainE < mini.gainE or mini.gainE==-1:
+            mini=node
+    finish =activatearound(grid,(mini.row,mini.colum),posE,posS)
+
+    
     return finish
 
-def findWay(grid,posE,posS):
+def recFindWay(grid,posS,node):
+    if (node.row,node.colum)==posS:
+        return True
+    else:
+        node.color=blue
+        recFindWay(grid,posS,node.origin)
+
+
+def findWay(grid,posE,posS,finish):
     grid[posE[0]][posE[1]].color=blue
+    recFindWay(grid,posS,finish)
+    '''
     best = Node(-1, -1, -1,-1)
     best.totgain=-1
     for i in range(-1,2):
@@ -168,10 +188,10 @@ def findWay(grid,posE,posS):
                 return 0
             if posE[0]+i>=0 and posE[0]+i<rows and posE[1]+j>=0 and posE[1]+j<rows and grid[posE[0]+i][posE[1]+j].color==red :
                 if grid[posE[0]+i][posE[1]+j].totgain < best.totgain or best.totgain==-1 :
-                    best=grid[posE[0]+i][posE[1]+j]
+    
+                    best=grid[posE[0]+i][posE[1]+j]'''
     
     
-    findWay(grid,(best.row,best.colum),posS)
  
 def main():
     
@@ -235,13 +255,13 @@ def main():
         elif state == 'finding':
             finish=algo(grid,posE,posS)
         elif state=='find way':
-            findWay(grid,posE,posS)
+            findWay(grid,posE,posS,finish)
             state='finish'
             print('tessend')
             finish=0
 
 
-        if finish==1:
+        if finish!=False:
             state= 'find way'
         if state=='finish':
             if keys[pg.K_RETURN]:
